f60080a501864b89405bb73fb31d9b4f3e164a2f
korichh <akopyankor@gmail.com>
2025-05-14 00:01:32 +0300

    fix: update deploy base url

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,16 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import { createBrowserRouter } from "react-router";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Home,
-  },
-  {
-    path: "*",
-    Component: NotFound,
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Home,
+    },
+    {
+      path: "*",
+      Component: NotFound,
+    },
+  ],
+  { basename: import.meta.env.BASE_URL }
+);
